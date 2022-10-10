f1bf284ae56f00bde7549c5c7522f1ae6eb2d192
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-10-10 13:48:22 +0100

    Add files via upload
    add string reader and iterator from start to end and vice versa

---
+++ PalindromeApp/PalindromeApp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        string word = "";
+        
+        Console.WriteLine("Please input word");
+        word = Console.ReadLine();
+        char[] wordCharacters = word.ToCharArray();
+        
+        //Print char array
+        for (int i = 0; i < wordCharacters.Length ; i++)
+        {
+            Console.Write("from start: "); 
+            Console.Write(wordCharacters[i] + " "); 
+            
+            Console.Write(" ; from end: ");
+            Console.Write(wordCharacters[ (wordCharacters.Length -1 ) - i] + " "); 
+            
+            Console.WriteLine("");
+        }
+    }
+}
