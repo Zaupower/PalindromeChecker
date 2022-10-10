fc04aa0990581b45464207dbb0b84803a5620f84
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-10-10 13:59:12 +0100

    Add files via upload

--- PalindromeApp/PalindromeApp/Program.cs
+++ PalindromeApp/PalindromeApp/Program.cs
@@ -7,18 +7,22 @@ class Program
         
         Console.WriteLine("Please input word");
         word = Console.ReadLine();
+
+        bool isPalindrome =  ispalidrome(word);
+        Console.WriteLine(word + (isPalindrome == true ? " IS" : " IS NOT") + " palidrome");
+
+    }
+
+    public static bool ispalidrome(String word)
+    {
         char[] wordCharacters = word.ToCharArray();
-        
-        //Print char array
         for (int i = 0; i < wordCharacters.Length ; i++)
         {
-            Console.Write("from start: "); 
-            Console.Write(wordCharacters[i] + " "); 
-            
-            Console.Write(" ; from end: ");
-            Console.Write(wordCharacters[ (wordCharacters.Length -1 ) - i] + " "); 
-            
-            Console.WriteLine("");
+            if (wordCharacters[i] != wordCharacters[ (wordCharacters.Length -1 ) - i] )
+            {
+                return false;
+            }
         }
+        return true;
     }
 }
